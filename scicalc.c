af8dc24e1b7f1d1d579b5b63f8551f16ebf397c7
Bkiser2000 <155510865+Bkiser2000@users.noreply.github.com>
2025-12-02 13:41:00 -0700

    Add files via upload

--- scicalc.c
+++ scicalc.c
@@ -1,15 +1,19 @@
+/* Compile using this command (On most Debian systems, I have to use it on my Debian system):
+   gcc scicalc.c -o scicalc -I/usr/include/python3.13 -L/usr/lib/x86_64-linux-gnu -lpython3.13 -lgsl -lgslcblas -lm
+   Use the -lgsl -lgslcblas to properly link gsl during compilation like this:
+   gcc example.c -o example -lgsl -lgslcblas -lm
+*/
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <Python.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_integration.h>
 #include <gsl/gsl_deriv.h>
 //#include "scicalc.h"
 
 #define PI 3.14159
-
-// Use the -lgsl -lgslcblas to properly link gsl during compilation like this:
-// gcc example.c -o example -lgsl -lgslcblas -lm
+#define PY_SSIZE_T_CLEAN
 
 int factorial(int num1);
 
@@ -19,17 +23,30 @@ int main() {
     double adjusted_value, decimal_rshift;
     int cnt = 0;
     
-    printf("Basic Calculator\n");
-    printf("1. Add\n");
-    printf("2. Subtract\n");
-    printf("3. Multiply\n");
-    printf("4. Divide\n");
-    printf("5. Square Root\n");
-    printf("6. Power\n");
-    printf("7. Arccosine (in radians)\n");
-    printf("8. Arccosine (hyperbolic)\n");
-    printf("9. Arcsine (in radians)\n");
+    printf("Advanced Scientific Calculator\n");
+    printf("1.  Add\n");
+    printf("2.  Subtract\n");
+    printf("3.  Multiply\n");
+    printf("4.  Divide\n");
+    printf("5.  Square Root\n");
+    printf("6.  Power\n");
+    printf("7.  Arccosine (in radians)\n");
+    printf("8.  Arccosine (hyperbolic)\n");
+    printf("9.  Arcsine (in radians)\n");
     printf("10. Cubic Root\n");
+    printf("11. Arctangent (in radians)\n");
+    printf("12. Arcsine (hyperbolic)\n");
+    printf("13. Angle theta (from the conversion of rectangular coordinates (x, y) to polar coordinates (r, theta))\n");
+    printf("14. Arctangent (hyperbolic)\n");
+    printf("15. Round a number up to its nearest integer\n");
+    printf("16. Copysign (Returns the first floating point x with the sign of the second floating point y)\n");
+    printf("17. Cosine (in radians)\n");
+    printf("18. Factorial\n");
+    printf("19. Circumference (circles)\n");
+    printf("20. Radius (circles)\n");
+    printf("21. Area (circles)\n");
+    printf("22. Prime number checker\n");
+    printf("23. Algebraic Calculator (systems of equations)\n");
     printf("Enter your choice (1-10): ");
     scanf("%2d", &choice);
 
@@ -74,8 +91,8 @@ int main() {
             break;
         case 6:
             printf("Enter two numbers: ");
-            if (num2 > 0) {
             scanf("%lf %lf", &num1, &num2);
+            if (num2 > 0) {
             result = pow(num1, num2);
             printf("Result: %2lf\n", result);
             } else {
@@ -158,7 +175,7 @@ int main() {
             break;
         case 16:
             printf("Enter two numbers: ");
-            scanf("%lf %lf", &num1, num2);
+            scanf("%lf %lf", &num1, &num2);
             result = copysign(num1, num2);
             printf("Result: %2lf", result);
             break;
@@ -206,7 +223,7 @@ int main() {
             cnt = 0;
 
             if (num_int <= 1) {
-            printf("%d is NOT prime", num1);
+            printf("%d is NOT prime", num_int);
             } else {
                 for (int i = 2; i <= num_int; i++) {
                     if (num_int % i == 0)
@@ -218,6 +235,42 @@ int main() {
                     printf("%d is prime", num_int);
                 }
             }
+            break;
+        case 23:
+        Py_Initialize();
+        PyRun_SimpleString("import sys; sys.path.insert(0, '.')");
+        PyObject *pName = PyUnicode_DecodeFSDefault("algie");
+        PyObject *pModule = PyImport_Import(pName);
+        Py_XDECREF(pName);
+       
+        if (pModule != NULL) {
+            PyObject *pFunc = PyObject_GetAttrString(pModule, "algebraic_calculator");
+
+            if (pFunc && PyCallable_Check(pFunc)) {
+                PyObject *pValue =PyObject_CallObject(pFunc, NULL);
+
+                if (pValue != NULL) {
+                    printf("%s\n", PyUnicode_AsUTF8(pValue));
+                    Py_XDECREF(pValue);
+
+                } else {
+                    PyErr_Print();
+                    fprintf(stderr, "Call failed\n");
+                }
+
+            } else {
+                PyErr_Print();
+                fprintf(stderr, "Cannot find function\n");
+            }
+            Py_XDECREF(pFunc);
+            Py_XDECREF(pModule);
+
+        } else {
+            PyErr_Print();
+            fprintf(stderr, "Failed to load\n");
+        }
+        
+        Py_Finalize();
             break;
         default:
             printf("Invalid choice\n");
